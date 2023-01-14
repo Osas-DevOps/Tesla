d9318d0ebe0a869b04c99b5e3ecba4a1c4909d8f
Osas-DevOps <cliffeg1@gmail.com>
2023-01-14 10:43:48 -0500

    this is a python3 script commit

---
+++ release.py
@@ -0,0 +1,3 @@
+#Let us create a python script with scriptName and finally move the script to remoterepo for further collaborations.
+#Let us call the python script release.py
+print ("this script is for practise purpose")
