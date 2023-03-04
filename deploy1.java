cd589046fe25585bc1210d2619ffcd6cc2de15ad
Osas-DevOps <cliffeg1@gmail.com>
2023-03-04 09:05:02 -0500

    deploy to stage env

--- deploy1.java
+++ deploy1.java
@@ -7,4 +7,7 @@ deployment5 just commenced
 deployment7 onboarded
 deployment8 on the run
 deployment6 has been trigered
->>>>>>> dev
+deploy to stage env
+deploy to dev env
+deploy java application
+deploy python application
